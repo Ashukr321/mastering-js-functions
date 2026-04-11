37eb0cece55cde52b33d72849e53b3db093d5eb4
Ashukr321 <coderashukr321@gmail.com>
2026-04-11 16:51:09 +0530

    feat(arrow function):understand the inline arrow function and usecase in the react

---
+++ 3. Arrow Function/app.js
@@ -0,0 +1,43 @@
+// arrow function : this is also  in hoisted  in nature : 
+//  es6 features 
+
+// this is also know as fat arrow function  
+// no own this 
+// widely use as callback function 
+// in the arrow function the context of this keywords take from its parent scope 
+// lexical scope 
+
+
+const printData = (...numbers)=>{
+    // rest parameters 
+
+    for(let i = 0; i < numbers.length; i++){
+        console.log(numbers[i]);
+    }
+}
+
+// rest parameters 
+printData(1,2,3,4,4);
+// arrow function widely use  in the functional components in the react 
+
+// it help to write clean code in the react due to this we prefer to use the 
+// arrow function in the react to crate the components  
+
+// inline arrow function  
+// in this case every time new function created due to this re-render component 
+// better approach move function outside the jsx 
+// when we move outside the inline arrow function function reference is same this will prevent unnnecessary re-rendering the components 
+
+
+<Button onClick={handleClick(id)}/> // this wrong ways to pass the argument 
+// this execute immediately during render 
+
+//  so inline function in  not always bad now now in this case we have to use this inline arrow funct
+// ion 
+
+{/* <Button onClick={()=>handleClick2(id)}/> */}
+/// pass the data vai props  or in small we can use the inline arrow function 
+
+
+
+
