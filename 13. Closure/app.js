88c12c3943370066f08f7674a185d5128dbc1ead
Ashukr321 <coderashukr321@gmail.com>
2026-04-12 00:55:33 +0530

    feat(closure):understand  the closure concept

---
+++ 13. Closure/app.js
@@ -0,0 +1,6 @@
+// closure is the most important concept in the javascript 
+// debouncing  closure used 
+// when outer function get executed but inner function still remembered the 
+// outer function variable which is created during creation phase 
+// function + its' lexical scope environment know it form closure 
+
