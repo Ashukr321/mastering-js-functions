99b84ac1dc062d6beb106fc9f55ebee607b87002
Ashukr321 <coderashukr321@gmail.com>
2026-04-12 00:30:37 +0530

    feat(named function )

---
+++ 10. Named Function Expression/app.js
@@ -0,0 +1,6 @@
+//named function is simple a function that has to be named assign 
+// debugging becomes easy 
+// help in recursive calling the function 
+//  named function expression  that also has a named to the function 
+
+// 
